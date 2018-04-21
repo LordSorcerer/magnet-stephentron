41fbb47f64b159e9500dce6c2c7388e0bbf83cfe
LordSorcerer <ms1@thousandstorms.com>
2018-04-21 00:40:02 -0400

    Heroku deploy test 2

--- magnet-stephentron.js
+++ magnet-stephentron.js
@@ -4,6 +4,19 @@ const botToken = require('./bot-token.json').token;
 // Create an instance of a Discord discordClient
 const discordClient = new Discord.Client();
 
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
 // Log our bot in
 discordClient.login(botToken);
 
